12860fb088dcfbd62d96d05200ee4cebc00af7cf
Bảo Nguyên <baonguyenyam@gmail.com>
2018-07-04 23:58:43 +0700

    add server

--- app.js
+++ app.js
@@ -111,6 +111,9 @@ app.use((req, res, next) => {
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
+app.get('/', (req, res) => {
+	res.redirect('/admin/');
+})
 /**
  * Primary app routes.
  */
